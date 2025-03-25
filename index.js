3467cc16e5a3cfc54bd396c278516ca88976fb9c
DevWizard82 <anasberrqia25@gmail.com>
2025-03-25 22:46:59 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const buttons = document.querySelectorAll(".drum");
+let audioNames = {
+  w: "tom-1",
+  a: "tom-2",
+  s: "tom-3",
+  d: "tom-4",
+  j: "crash",
+  k: "kick-bass",
+  l: "snare",
+};
+
+function playsound(sound) {
+  let audio = new Audio(`sounds/${sound}.mp3`);
+  audio.play();
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    let buttoninnerHTML = button.innerHTML;
+    if (audioNames[buttoninnerHTML]) {
+      playsound(audioNames[buttoninnerHTML]);
+    }
+  });
+});
+
+document.addEventListener("keydown", (event) => {
+  let keyPressed = event.key.toLowerCase();
+  if (audioNames[keyPressed]) {
+    playsound(audioNames[keyPressed]);
+  }
+});
